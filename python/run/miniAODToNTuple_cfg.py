78391c3d448aef62a4de81f3e86b5be3cae4302b
Douglas Burns <db0268@bristol.ac.uk>
2017-01-24 15:35:40 +0000

    Add badMuon / badChargedHadron Filters

--- python/run/miniAODToNTuple_cfg.py
+++ python/run/miniAODToNTuple_cfg.py
@@ -88,9 +88,9 @@ if isTTbarMC:
 from BristolAnalysis.NTupleTools.pseudoTopConfig_cff import setupPseudoTop
 setupPseudoTop(process, cms)
 
-# Rerun HBHE filter and others
-from BristolAnalysis.NTupleTools.metFilters_cfi import setupMETFilters
-setupMETFilters(process, cms)
+# # Rerun HBHE filter and others
+# from BristolAnalysis.NTupleTools.metFilters_cfi import setupMETFilters
+# setupMETFilters(process, cms)
 
 # Rerun MET
 from BristolAnalysis.NTupleTools.MET_Setup_cff import setup_MET
@@ -104,6 +104,9 @@ setup_jets(process, cms, options)
 from BristolAnalysis.NTupleTools.Electron_Setup_cff import setup_electrons
 setup_electrons(process, cms, options)
 
+# Bad Muon Filters
+from BristolAnalysis.NTupleTools.Muon_Setup_cff import setup_muons
+setup_muons(process, cms, options)
 
 # Load the selection filters and the selection analyzers
 process.load('BristolAnalysis.NTupleTools.muonSelections_cff')
@@ -137,6 +140,7 @@ process.load('BristolAnalysis.NTupleTools.userdata.EventUserData_cff')
 if isTTbarMC:
     process.makingNTuples = cms.Path(
         # process.metFilters *
+        process.badMuonTagger *
         process.smearedElectrons *
         process.egmGsfElectronIDSequence *
         process.reapplyJEC *
@@ -156,6 +160,7 @@ if isTTbarMC:
 else:
     process.makingNTuples = cms.Path(
         # process.metFilters *
+        process.badMuonTagger *
         process.smearedElectrons *
         process.egmGsfElectronIDSequence *
         process.reapplyJEC *
