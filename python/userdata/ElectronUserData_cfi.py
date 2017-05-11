0c4a1e793f278272e530869f645908c4458d1354
Douglas Burns <db0268@bristol.ac.uk>
2017-05-11 10:48:56 +0100

    Synchronise Veto Selection

--- python/userdata/ElectronUserData_cfi.py
+++ python/userdata/ElectronUserData_cfi.py
@@ -18,7 +18,7 @@ electronUserData = cms.EDProducer(
         "egmGsfElectronIDs:cutBasedElectronHLTPreselection-Summer16-V1"),
     # Top Object Definitions
     minVetoElectronPt=cms.double(15.),
-    maxVetoElectronEta=cms.double(2.1),
+    maxVetoElectronEta=cms.double(2.4),
     minSignalElectronPt=cms.double(34.),
     maxSignalElectronEta=cms.double(2.1),
     ebRecHits = cms.InputTag("reducedEgamma","reducedEBRecHits"),
